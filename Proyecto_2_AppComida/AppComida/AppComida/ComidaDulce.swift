5957b88c4b1011f5911fe7db7c1069bacbe2fb62
FaustoAlejandro89 <31267240+FaustoAlejandro89@users.noreply.github.com>
2019-03-11 15:56:31 -0600

    Edición del programa
    Aquí hay que editar el programa

---
+++ Proyecto_2_AppComida/AppComida/AppComida/ComidaDulce.swift
@@ -0,0 +1,15 @@
+//
+//  ComidaDulce.swift
+//  AppComida
+//
+//  Created by Macbook on 3/11/19.
+//  Copyright © 2019 Apples Inc. All rights reserved.
+//
+
+import Foundation
+
+struct Comida{
+    var nombre: String
+    var cantidad: String
+    var foto: String
+}
---
+++ Proyecto_2_AppComida/README.md
@@ -0,0 +1,2 @@
+# Proyecto_2_AppComida
+Segundo proyecto de MPOO
