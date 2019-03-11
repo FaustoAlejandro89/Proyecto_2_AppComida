c9bd619b8f26d3d4da38971c8262c4c3c294fd18
FaustoAlejandro89 <31267240+FaustoAlejandro89@users.noreply.github.com>
2019-03-11 16:06:07 -0600

    Update ViewController.swift

--- AppComida/AppComida/ViewController.swift
+++ AppComida/AppComida/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var tablita: UITableView!
     @IBOutlet weak var imagen: UIImageView!
     var dato = Comida(nombre: "Algo", cantidad: "12", foto: "algo")
+    //Hola mundo
     
     override func viewDidLoad() {
         super.viewDidLoad()
